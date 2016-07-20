b6773ed690891ac7e2ce0dbd5c8ea3217bc60326
薛纪昌 <416908010@qq.com>
2016-07-20 14:22:06 +0800

    Add files via upload

---
+++ xinyu/Demo/Demo.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+	
+	for (var i = 16; i >= 0; i--) {
+		var opt = document.createElement('option');
+		var sel = document.getElementById('year');
+		opt.appendChild(document.createTextNode(2000+i));
+		opt.setAttribute('value',"123");
+		sel.appendChild(opt);
+	}
+}
